8fc4a7dfb34b19abffff15d3a286b4fdbbac21c7
imbasri <imbasri.dev@gmail.com>
2024-01-31 10:11:53 +0700

    update - change keymaps , add which-key
    hapus keybinds resume , add which-key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
-vim.keymap.set(
-    "n",
-    "<leader>sx",
-    require("telescope.builtin").resume,
-    { noremap = true, silent = true, desc = "Resume" }
-)
-
+-- vim.keymap.set(
+--     "n",
+--     "<leader>sx",
+--     require("telescope.builtin").resume,
+--     { noremap = true, silent = true, desc = "Resume" }
+-- )
+--
 --automatically comment with neogen
-local opts = { noremap = true, silent = true,desc="Neogen Comments" }
+local opts = { noremap = true, silent = true, }
 vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate()<CR>", opts)
